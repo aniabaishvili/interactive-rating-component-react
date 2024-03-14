e6724b8234eb37d4050850aef4e6b1efcb49131e
ani <aniabaishvili1@gmail.com>
2024-03-14 16:50:40 +0400

    finish rate container

--- src/components/rate/Rate.jsx
+++ src/components/rate/Rate.jsx
@@ -1,9 +1,23 @@
-import './Rate.css'
+import "./Rate.css";
+import logo from "/images/icon-star.svg";
 
-function Rate(){
-    return(
-        <div></div>
-    )
+function Rate() {
+    let buttons = [1,2,3,4,5]
+  return (
+    <div className="rate-container">
+      <img src={logo}/>
+      <h1>How did we do?</h1>
+      <p>
+        Please let us know how we did with your support request. All feedback is
+        appreciated to help us improve our offering!
+      </p>
+      <div className="btn-container">
+       {buttons.map((button) => <button className="rate-btn">{button}</button>)}
+      </div>
+      <button className="submit">SUBMIT</button>
+      
+    </div>
+  );
 }
 
-export default Rate
+export default Rate;
